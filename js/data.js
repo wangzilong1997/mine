d2c220babb431eb02d2ca06a8a73e23fad518239
wangzilong <1321787581@qq.com>
2019-07-15 17:41:05 +0800

    update

--- js/data.js
+++ js/data.js
@@ -17,7 +17,7 @@ wss.data = {
 	'contact' : [
 	'联系方式',
 	'QQ:1321 787 581',
-	'TEL:152227111790',
+	'TEL:15227111790',
 	],
 	'skills' : [
 	'技术了解',
@@ -29,7 +29,84 @@ wss.data = {
 	'photoshop premiere 相关软件使用'
 	],
 	'timeline' : [
+	{
+	  'class' : 'r',
+	  'date'  : '曾用过的简单程序',
+	  'event' : '简单爬虫，selenium 程序',
+	  'href'  : 'https://github.com/wangzilong1997/script'
+	},
+	{ 'class' : 'r',
+	  'date'  : '最近的简单的网站项目',
+	  'event' : '简单的项目',
+	  'href'  : 'https://github.com/wangzilong1997/The-Story-of-Data'
+	},
+	{
+	  'class' : 'r',
+	  'date'  : '可以完成简单图片处理、视频剪辑、效果制作(简单的)',
+	  'event' : 'photoshop、 premiere、after effects',
+	  'href'  : ''
+	},
+	{
+	  'class' : 'l',
+	  'date'  : '微信后台实现简单逻辑功能',
+	  'event' : '微信公众号后台开发、简单小程序开发',
+	  'href'  : 'https://github.com/wangzilong1997/vx'
+	},
+	{
+	  'class' : 'r',
+	  'date'  : 'Grunt可以实现单元测试自动构建等，Webpack简单打包。。',
+	  'event' : 'Grunt、Webpack自动构建打包工具、Git',
+	  'href'  : '//github.com/wangzilong1997/spa'
+	},
+	{
+	  'class' : 'r',
+	  'date'  : '能够使用进行简单开发(依次忘得多)',
+	  'event' : 'React react-redux、Angular、Inoic、',
+	  'href'  : '//github.com/wangzilong1997/reactwork'
+	},
+	{
+	  'class' : 'l',
+	  'date'  : '简单实现数据库存储查询需求(简单的都会，太复杂的不会/)',
+	  'event' : 'MYSQL MongoDB',
+	  'href'  : ''
+	},
+	{
+	  'class' : 'r',
+	  'date'  : '可以看懂简单逻辑代码',
+	  'event' : 'C++、Java',
+	  'href'  : ''
+	},
+	{
+	  'class' : 'r',
+	  'date'  : 'Nodejs、Python可作为后台语言进行开发',
+	  'event' : 'Nodejs、Python轻量语言',
+	  'href'  : ''
+	},
+	{
+	  'class' : 'r',
+	  'date'  : '熟悉Linux、可以用来开发',
+	  'event' : 'Linux环境应用开发',
+	  'href'  : ''
+	}, 
+	{
+	  'class' : 'l',
+	  'date'  : '了解掌握',
+	  'event' : 'XML、JSON、AJAX、HTTP',
+	  'href'  : ''
+	},
+	{
+	  'class' : 'l',
+	  'date'  : '基本掌握',
+	  'event' : 'jQuery、Boostrap基本掌握',
+	  'href'  : ''
+	},
     {
+	  'class' : 'l',
+	  'date'  : '掌握',
+	  'event' : 'HTML、CSS、Javascript基本技术',
+	  'href'  : ''
+	},
+	{
       'class' : 'r',
       'date'  : '2016年9月1日',
       'event' : '河北师范大学入学',
